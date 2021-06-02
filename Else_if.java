430ef63132fa609ae2b689253ced926dd34396a3
Mukilavan <53076357+Mukilavan@users.noreply.github.com>
2021-06-02 19:19:13 +0530

    Update Else_if.java

--- Else_if.java
+++ Else_if.java
@@ -5,7 +5,6 @@ public class Solution {
     public static void main(String[]args)
      {
           int num;
-          System.out.println("num");
            Scanner sc= new Scanner (System.in);
            num= sc.nextInt();
            if(num%2!=0)
